2f2dfe5672f2dc89cb3d4556615fb25d63815628
Linh Tran <hlinh.tran26@gmail.com>
2024-10-08 15:03:58 -0700

    fix bug in simulation code

--- run_sim_analysis.sh
+++ run_sim_analysis.sh
@@ -6,3 +6,7 @@ python simulation_analysis.py S2_S26_2 0.3 sum
 python simulation_analysis.py S2_S26_2 0.4 bin 30
 python simulation_analysis.py S2_S26_2 0.4 bin 49 # last bin due to 0-index
 python simulation_analysis.py S2_S26_2 0.025 range 15 33
+
+# ran after bug fix
+python simulation_analysis.py S12_S22_3 0.2 sum
+python simulation_analysis.py S12_S22_3 0.2 bin 27
